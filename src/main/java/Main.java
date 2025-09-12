9d8b909113ff8550d9f13e9a79dbca4b84a3dcc9
Martin Naychev <martin.naychev@gmail.com>
2025-09-12 16:34:05 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import entities.Town;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+
+import java.util.List;
 
 
 public class Main {
@@ -9,8 +12,15 @@ public class Main {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
 
+        List<Town> resultList = em.createQuery("SELECT t FROM Town as t", Town.class).getResultList();
 
-
+        for (Town town : resultList) {
+            if (town.getName().length() > 5) {
+                em.detach(town);
+            }else {
+                town.setName(town.getName().toUpperCase());
+            }
+        }
 
         em.getTransaction().commit();
         em.close();
