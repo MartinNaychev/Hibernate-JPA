e0a8ea1e58f3e9aced830fa59567449b4cee61b7
Martin Naychev <martin.naychev@gmail.com>
2025-09-17 13:07:29 +0300

    Create StoreLocation.java

---
+++ Exercise/src/main/java/org/example/entities/SalesDatabase/StoreLocation.java
@@ -0,0 +1,47 @@
+package org.example.entities.SalesDatabase;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "store_locations" )
+public class StoreLocation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "location_name")
+    private String locationName;
+
+    @OneToMany(mappedBy = "storeLocation")
+    private List<Sale> sales;
+
+    public StoreLocation() {
+    }
+
+    public List<Sale> getSales() {
+        return sales;
+    }
+
+    public void setSales(List<Sale> sales) {
+        this.sales = sales;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+}
